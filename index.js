dd5905a078a0df4e2bba102eaf4ec8d218fe2ebc
GeoffreyOgembo <geoffereyogembo06@gmail.com>
2022-06-13 10:19:18 -0400

    message

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+function findMatching(drivers, driver){
+let newArray= drivers.filter(val => driver === val || driver.toLowerCase () === val)
+    return newArray
+  
+}
+function fuzzyMatch(drivers,firstTwo){
+let newArray1 = drivers.filter(rslt =>rslt.slice(0,2)===firstTwo)
+return newArray1
+}
+const matchName = (drivers, str) => {
+    const filterArr = drivers.filter((filtered) => {
+        console.log(filtered.name);
+        return filtered.name === str;
+    });
+    return filterArr;
+} 
